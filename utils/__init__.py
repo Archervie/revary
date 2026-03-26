14f1fd0bf2a02cf93a187b7a6539805191a9a029
Archervie <dyquezada06@gmail.com>
2026-03-26 18:43:03 -0400

    fixed directory structure

--- utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .log_utils import Logger
-from .auth_utils import Authorization, is_authorized, UnauthorizedError
--- utils/log_utils.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-class Logger:
-    def __init__(self) -> None:
-        pass
-
-    def logger(self, name) -> logging.Logger:
-        return logging.getLogger(name)
